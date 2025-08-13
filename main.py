18b2d3ef3fa7574e0e801793530c4bd5c1db9c0d
Joseph Richards <Richardsjr.Joseph@gmail.com>
2025-08-13 11:19:16 -0400

    PyCharm commit test

--- main.py
+++ main.py
@@ -2,3 +2,120 @@
 Following a guide to learn how to use a Convolutional Neural Network so that I
 can build my own image sorting application.
 """
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+
+import warnings
+warnings.filterwarnings('ignore')
+
+
+from tensorflow import keras
+from keras import layers
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Activation, Dropout, Flatten, Dense
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.utils import image_dataset_from_directory
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+
+import os
+import matplotlib.image as mpimg
+
+from PIL import Image
+
+from tensorflow.python.client import device_lib
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+print(device_lib.list_local_devices())
+
+data_path = os.path.join('PetImages')
+
+cat_dir = os.path.join(data_path, 'Cat')
+dog_dir = os.path.join(data_path, 'Dog')
+
+
+cat_names = os.listdir(cat_dir)
+dog_names = os.listdir(dog_dir)
+
+fig = plt.gcf()
+fig.set_size_inches(16, 16)
+
+pic_index = 210
+
+cat_images = [os.path.join(cat_dir, fname)
+              for fname in cat_names[pic_index-8:pic_index]]
+
+dog_images = [os.path.join(dog_dir, fname)
+              for fname in dog_names[pic_index-8:pic_index]]
+##########
+
+for i, img_path in enumerate(cat_images + dog_images):
+    sp = plt.subplot(4, 4, i+1)
+    sp.axis('Off')
+
+    img = mpimg.imread(img_path)
+    plt.imshow(img)
+
+plt.show()
+
+base_dir = 'PetImages'
+train_datagen = tf.keras.utils.image_dataset_from_directory(
+    base_dir,
+    image_size=(200,200),
+    subset='training',
+    seed=1,
+    validation_split=0.1,
+    batch_size=32
+)
+test_datagen = tf.keras.utils.image_dataset_from_directory(
+    base_dir,
+    image_size=(200,200),
+    subset='validation',
+    seed=1,
+    validation_split=0.1,
+    batch_size=32
+)
+
+model = tf.keras.models.Sequential([
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(200, 200, 3)),
+    layers.MaxPooling2D(2, 2),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D(2, 2),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D(2, 2),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D(2, 2),
+
+    layers.Flatten(),
+    layers.Dense(512, activation='relu'),
+    layers.BatchNormalization(),
+    layers.Dense(512, activation='relu'),
+    layers.Dropout(0.1),
+    layers.BatchNormalization(),
+    layers.Dense(512, activation='relu'),
+    layers.Dropout(0.2),
+    layers.BatchNormalization(),
+    layers.Dense(1, activation='sigmoid')
+])
+
+model.summary()
+
+model.compile(
+    loss='binary_crossentropy',
+    optimizer='adam',
+    metrics=['accuracy']
+)
+
+history = model.fit(train_datagen,
+          epochs=10,
+          validation_data=test_datagen)
+
+history_df = pd.DataFrame(history.history)
+history_df.loc[:, ['loss', 'val_loss']].plot()
+history_df.loc[:, ['accuracy', 'val_accuracy']].plot()
+plt.show()
